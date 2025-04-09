13ea2f21493df86512ddb51764a92f94640a2ad1
Nahid44332 <nahidhossen3278@gmail.com>
2025-04-09 13:49:13 +0600

    php crud Operation

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conn = new mysqli ('localhost', 'root', '', 'school_management_system');
+
+    // if($conn == true){
+    //     echo "Connected Successfully!";
+    // }
+    // else{
+    //     echo "Connected Failed!!";
+    // }
+?>
